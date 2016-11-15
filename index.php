07ba859d302765aa816e949f4e4f23173811bdfa
ethan <ethan.beaver@wallawalla.edu>
2016-11-14 18:41:05 -0800

    Remove print statements

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <?php
 require('vars.php');
 //index.php
-print("running php script");
 
 //Get dat simple saml library
 require_once('../../simplesamlphp/lib/_autoload.php');
-print("running require");
 
 //Create the simple saml instance
 $as = new SimpleSAML_Auth_Simple('default-sp');
-print("Declared simple saml");
 
 //Send user to login page
 $as->requireAuth();
-print("ran requireAuth()");
 
 //Get user attributes
 $attributes = $as->getAttributes();
-print_r($attributes);
 
 //Generate Token
 $now = time();
 $hash = hash_hmac('sha256',$attributes["employee_id"][0] . "|" . time(),$secret_key);
 
 $token = $attributes["employee_id"][0] . "|" . $now . "|" . $hash;
-print($token);
 
 // The data to send to the API
 $postData = array(
